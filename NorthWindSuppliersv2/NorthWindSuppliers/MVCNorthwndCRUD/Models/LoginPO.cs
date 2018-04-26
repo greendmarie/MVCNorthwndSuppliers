ddcea2b90f9eca977e109a29380af64c61c82bee
Dana Barnhill <38538648+greendmarie@users.noreply.github.com>
2018-04-26 17:00:05 -0500

    Commit 3
    Source files updated

---
+++ NorthWindSuppliersv2/NorthWindSuppliers/MVCNorthwndCRUD/Models/LoginPO.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MVCNorthwndCRUD.Models
+{
+    public class LoginPO
+    {
+        [Required]
+        [StringLength(50)]
+        public string Username { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string Password { get; set; }
+    }
+}
---
+++ NorthWindSuppliersv2/NorthWindSuppliers/MVCNorthwndCRUD/Models/UserPO.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MVCNorthwndCRUD.Models
+{
+    public class UserPO
+    {
+        public Int64 UserID { get; set; }
+
+        [Required]
+        [StringLength(20, MinimumLength = 5, ErrorMessage = "Please enter a Username between 5-20 characters")]
+        public string Username { get; set; }
+
+        [Required]
+        [StringLength(20, MinimumLength = 5, ErrorMessage = "Please enter a Password between 5-20 characters")]
+        public string Password { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string Firstname { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string Lastname { get; set; }
+
+        [StringLength(150)]
+        [EmailAddress]
+        public string EmailAddress { get; set; }
+
+        public int RoleID { get; set; }
+    }
+}
